687c81be1fa891ff677ca35e4cb80f738e66d9da
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-10-11 16:11:46 +0700

    Create Lingkaran.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Lingkaran.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author HP
+ */
+public class Lingkaran {
+    
+    int keliling(int jejari){
+        return (2 * 22/7 * jejari);
+    }
+    
+    double keliling(double jejari){
+        return (2 * 22/7 * jejari);
+    }
+    
+    int luas(int jejari){
+        return (22/7 * jejari * jejari);
+    }
+    
+    double luas(double jejari){
+        return (22/7 * jejari * jejari);
+    }
+}
