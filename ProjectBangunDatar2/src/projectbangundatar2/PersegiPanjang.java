d35fc440f052abab5f4b63d49ae5466ca7f04ebd
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-10-11 16:11:56 +0700

    Create PersegiPanjang.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/PersegiPanjang.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author HP
+ */
+public class PersegiPanjang {
+    
+    int keliling(int panjang, int lebar){
+        return (2 * (panjang + lebar));
+    }
+    
+    double keliling(double panjang, double lebar) {
+        return (2 * (panjang + lebar));
+    }
+    
+    double keliling(double panjang, int lebar) {
+        return (2 * (panjang + lebar));
+    }
+    
+    double keliling(int panjang, double lebar) {
+        return (2 * (panjang + lebar));
+    }
+
+    int luas(int panjang, int lebar){
+        return (panjang * lebar);
+    }
+    
+    double luas(double panjang, double lebar) {
+        return (panjang * lebar);
+    }
+    
+    double luas(double panjang, int lebar) {
+        return (panjang * lebar);
+    }
+    
+    double luas(int panjang, double lebar) {
+        return (panjang * lebar);
+    }
+
+}
