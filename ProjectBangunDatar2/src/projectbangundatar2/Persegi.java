673181e9c7d9b8e19416ee143eb096ec1fdb6434
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-10-11 16:11:50 +0700

    Create Persegi.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Persegi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author HP
+ */
+public class Persegi {
+    
+    int keliling(int sisi){
+        return (sisi * sisi * sisi * sisi);
+    }
+    
+    double keliling(double sisi){
+        return (sisi * sisi * sisi * sisi);
+    }
+    
+    int luas(int sisi){
+        return (sisi * sisi);
+    }
+    
+    double luas(double sisi){
+        return (sisi * sisi);
+    }
+}
