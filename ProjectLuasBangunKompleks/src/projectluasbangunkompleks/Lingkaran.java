071cfab7fab8fc28c3246368f95ef9dccd4ee7ab
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-10-11 16:13:34 +0700

    Create Lingkaran.java

---
+++ ProjectLuasBangunKompleks/src/projectluasbangunkompleks/Lingkaran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks;
+
+/**
+ *
+ * @author HP
+ */
+public class Lingkaran {
+    //atribut
+    int r;
+    
+    //method
+    int hitungLuas(){
+        int luas = 22/7 * this.r * this.r;
+        return luas;
+    }
+}
