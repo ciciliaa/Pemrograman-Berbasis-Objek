29444e29ed5d70659576e074bffe7c8ce7299b00
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-11-02 21:15:15 +0700

    Create Keuntungan.java

---
+++ project/src/project/Keuntungan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author HP
+ */
+public class Keuntungan {
+    int untung(int harga_jual, int buah){
+        int total = harga_jual * buah;
+        return total;
+    }
+
+}
