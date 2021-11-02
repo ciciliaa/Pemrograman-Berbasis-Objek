b817914cc43ba67435108740ae779ab219dc3d30
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-11-02 21:15:06 +0700

    Create Buah.java

---
+++ project/src/project/Buah.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author HP
+ */
+public class Buah {
+    int buah (int buah){
+        int total = buah * 8000;
+        return total;
+    }
+    
+}
