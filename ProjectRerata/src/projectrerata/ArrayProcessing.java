b83ef2e2344d8b05175edca64cacad633be10475
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-11-09 22:40:20 +0700

    Create ArrayProcessing.java

---
+++ ProjectRerata/src/projectrerata/ArrayProcessing.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectrerata;
+
+/**
+ *
+ * @author HP
+ */
+import java.util.Scanner;
+public class ArrayProcessing {
+    int [] dataBil = new int[100];
+    int n,a,j;
+    
+    void input(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan banyak data : ");
+        this.n = input.nextInt();
+            
+        for(a = 0; a < n; a++){
+            System.out.println("Masukan bil ke " + (a+1)+ " : ");
+            this.dataBil[a] = input.nextInt();
+        }
+    }
+    
+    double hitungRerata(int[] dataArray){
+        double sum = 0;
+        for(a=0;a<this.n;a++){
+            sum += dataArray[a];
+        }
+        
+        double rerata = sum/this.n;
+        return rerata;
+    }
+    
+    int cariMax(int[] dataArray){
+        int max = dataArray[0];
+        
+        for( a=0;a<this.n;a++){
+            if (max<dataArray[a]){
+                max = dataArray[a];
+            }
+        }
+        
+        return max;
+    }
+    
+    int cariMin(int[] dataArray){
+        int min = dataArray[0];
+        
+        for(a=0;a<this.n;a++){
+            if (min>dataArray[a]){
+                min = dataArray[a];
+            }
+        }
+        
+        return min;
+    }
+    
+    void output(){
+    System.out.println("Rerata      : " + this.hitungRerata(this.dataBil));
+    System.out.println("Nilai min   : " + this.cariMin(this.dataBil));
+    System.out.println("Nilai max   : " + this.cariMax(this.dataBil));
+    
+    }   
+}
