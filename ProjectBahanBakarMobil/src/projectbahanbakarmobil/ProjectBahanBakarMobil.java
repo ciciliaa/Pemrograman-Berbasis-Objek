88d561e33c4df52a633862172265f494480d73a4
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-12-07 22:33:19 +0700

    Create ProjectBahanBakarMobil.java

---
+++ ProjectBahanBakarMobil/src/projectbahanbakarmobil/ProjectBahanBakarMobil.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbahanbakarmobil;
+
+/**
+ *
+ * @author HP
+ */
+public class ProjectBahanBakarMobil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Sedan m1 = new Sedan();
+        m1.output();
+        
+        Suv m2 = new Suv();
+        m2.output();
+        
+        Mpv m3 = new Mpv();
+        m3.output();
+    }
+    
+}
